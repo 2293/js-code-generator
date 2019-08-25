0097b0eaf309b6faaa53135c150ee43ee6bf735a
a boy <a_boy@live.com>
2019-08-25 17:40:33 +0800

    micro.js

--- micro.js
+++ micro.js
@@ -1,8 +1,15 @@
+/*!
+ * micro.js v1.0.0
+ * (c) 2018-2019 Cody Luo
+ * Released under the MIT License.
+ */
+
 function _(id) {
     if (id == null) return document.body;
     if(Number.isInteger(id)) return document.body.children[id];
     return document.getElementById(id)
 }
+
 function $(sel) {
     return document.querySelector(sel);
 }
